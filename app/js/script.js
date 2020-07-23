abfd101393dfbc2c3c56864744ea51dee5630c16
William Decker <willdecker@gmail.com>
2020-07-23 01:46:00 -0400

    Build animated mobile menu button and overlay.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log('hello world!');
+const hamburgerBtn = document.querySelector('#hamburger-btn');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+hamburgerBtn.addEventListener('click', function () {
+  if (header.classList.contains('open')) {
+    // Close mobile menu
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  } else {
+    // Open mobile menu
+    header.classList.add('open');
+    overlay.classList.add('fade-in');
+    overlay.classList.remove('fade-out');
+  }
+});
