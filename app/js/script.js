d7c6eb4749660d22f3ae89a65c59f3a60cc2ccd1
William Decker <willdecker@gmail.com>
2020-07-25 12:33:25 -0400

    Build animated overlay and mobile nav.

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,25 @@
 const hamburgerBtn = document.querySelector('#hamburger-btn');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 hamburgerBtn.addEventListener('click', function () {
   if (header.classList.contains('open')) {
     // Close mobile menu
+    body.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElems.forEach((element) => {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
   } else {
     // Open mobile menu
+    body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.add('fade-in');
-    overlay.classList.remove('fade-out');
+    fadeElems.forEach((element) => {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
   }
 });
